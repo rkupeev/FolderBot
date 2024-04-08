4acfa0c49cc1d21ec673812a7d1eede1222524af
Ruslan K. <r-kupeev@mail.ru>
2024-04-08 23:51:45 +0300

    core directory was added
    - Loading of bot settings has been moved from config.py , described on the same level as __main__, in settings.py

---
+++ app/core/settings.py
@@ -0,0 +1,40 @@
+from functools import lru_cache
+from pathlib import Path
+
+from aiogram.enums import ParseMode
+from pydantic_settings import BaseSettings, SettingsConfigDict
+from pydantic import DirectoryPath, SecretStr
+
+
+ROOT_DIR: DirectoryPath = Path(__file__).parent.parent
+
+
+class BotSettings(BaseSettings):
+    model_config = SettingsConfigDict(env_file=f'{ROOT_DIR}/.env')
+    BOT_TOKEN : SecretStr
+    PARSE_MODE: ParseMode | str = ParseMode.HTML
+
+
+class DatabaseSettings(BaseSettings):
+    model_config = SettingsConfigDict(env_file=f'{ROOT_DIR}/.env')
+    DB_HOST: str
+    DB_PORT: int
+    DB_USER: str
+    DB_PASS: str
+    DB_NAME: str
+
+    @property
+    def DATABASE_URL_psycopg(self):
+        # DSN
+        # postgresql+psycopg://postgres:postgres@localhost:5432/sa
+        return f"postgresql+psycopg://{self.DB_USER}:{self.DB_PASS}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
+
+
+class Settings():
+    bot: BotSettings = BotSettings()
+    database: DatabaseSettings = DatabaseSettings()
+
+
+@lru_cache
+def get_settings() -> Settings:
+    return Settings()
