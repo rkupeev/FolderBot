be56a111e86f4178e1bf37dfc17b4c0345ac9cef
Ruslan K. <r-kupeev@mail.ru>
2024-05-08 01:13:01 +0300

    A piece of code responsible for creating tables in the database has been written

--- app/database/connection.py
+++ app/database/connection.py
@@ -1,40 +1,21 @@
 import asyncio
-from sqlalchemy import MetaData, Table, Integer, String, Column, ForeignKey, DateTime, Text
-from sqlalchemy.ext.asyncio import create_async_engine
-
-from sqlalchemy import MetaData, Table, Integer, String, Column, ForeignKey, DateTime, Text
-from sqlalchemy.ext.declarative import declarative_base 
-from datetime import datetime
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 
 from app.core import get_settings
+from app.database.models import metadata 
 
-#Base = declarative_base()
 
-metadata = MetaData()
-
-#
-async def async_main():
-    engine = create_async_engine(
+async def create_tables():
+    async_engine = create_async_engine(
         get_settings().database.DATABASE_URL_asyncpg
         )
-    async with engine.begin() as conn:
+    async_session = AsyncSession(async_engine)
+
+    async with async_engine.begin() as conn:
         await conn.run_sync(metadata.drop_all)
         await conn.run_sync(metadata.create_all)
-        #await Base.metadata.create_all(engine)
 
-'''
-class Users(Base):
-    __tablename__ = 'users'
-    user_id = Column(Integer, primary_key=True)
-    telegram_id = Column(Integer, nullable=False, unique=True)
-    reg_date = Column(DateTime(), default=datetime.now)
+    await async_session.commit()
+    await async_session.close()
 
-
-class Entries(Base):
-    __tablename__ = 'entries'
-    entry_id = Column(Integer, primary_key=True)
-    user_id = Column(Integer, ForeignKey("users.user_id"))
-    heading = Column(Text(128), nullable=False)
-    entry = Column(Text(2048), nullable=False)'''
-
-asyncio.run(async_main())
+asyncio.run(create_tables())
