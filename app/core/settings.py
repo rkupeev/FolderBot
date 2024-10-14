e4bb640806f84529ee9ac98aebdf9a517bb01588
Ruslan K. <r-kupeev@mail.ru>
2024-10-14 23:42:23 +0300

    Added functions for extracting settings from an env file

---
+++ app/core/settings.py
@@ -0,0 +1,38 @@
+
+from functools import lru_cache
+from aiogram.enums import ParseMode
+from pydantic_settings import BaseSettings, SettingsConfigDict
+from pydantic import DirectoryPath, SecretStr
+
+#ROOT_DIR: DirectoryPath = Path(__file__).parent.parent
+
+
+class BotSettings(BaseSettings):
+    model_config = SettingsConfigDict(env_file=f'/.env')
+    BOT_TOKEN: SecretStr
+    PARSE_MODE: ParseMode | str = ParseMode.HTML
+
+
+
+class DatabaseSettings(BaseSettings):
+    model_config = SettingsConfigDict(env_file=f'/.env')
+    DB_HOST: str
+    DB_PORT: int
+    DB_USER: str
+    DB_PASS: str
+    DB_NAME: str
+
+    @property
+    def DATABASE_URL_asyncpg(self):
+        # postgresql+asyncpg://postgres:postgres@localhost:5432/sa
+        return f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASS}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
+    
+
+class Settings():
+    bot: BotSettings = BotSettings()
+    database: DatabaseSettings = DatabaseSettings()
+
+
+@lru_cache
+def get_settings() -> Settings:
+    return Settings()
