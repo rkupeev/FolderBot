b244c31519b8b0888e8ba6e5bdb70b0441ac589b
Ruslan Kupeev <rkupeev@users.noreply.github.com>
2025-03-29 21:55:35 +0300

    Routers of handlers are moved to a separate file
    Now routers connect to the dispatcher as follows:
    
        from app.handlers.users.routers import user_router
        dp = Dispatcher()
        dp.include_router(user_router)

---
+++ app/handlers/users/routers.py
@@ -0,0 +1,9 @@
+from aiogram import Router
+from app.handlers.users.start_handler import start_router
+from app.handlers.users.help_handler import help_router
+from app.handlers.users.menu_handler import menu_router
+
+user_router = Router()
+user_router.include_router(start_router)
+user_router.include_router(help_router)
+user_router.include_router(menu_router)
