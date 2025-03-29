5cabee8b1bb9a416a16598f8a459bf853d409d97
Ruslan Kupeev <rkupeev@users.noreply.github.com>
2025-03-29 22:14:26 +0300

    File new_entity_handler has been created

---
+++ app/handlers/users/new_entity_handler.py
@@ -0,0 +1,11 @@
+from aiogram import Router, F
+from aiogram.filters import Command
+from aiogram.types import Message, ReplyKeyboardRemove
+
+from aiogram.enums import ParseMode
+
+from aiogram.fsm.context import FSMContext
+
+
+
+
