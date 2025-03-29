6564025e0c63b0a05c92283f1bc490b68586d406
Ruslan Kupeev <rkupeev@users.noreply.github.com>
2025-03-29 22:11:47 +0300

    Added several changes
    - Loading settings into a separate variable inside the main function
    - The list of routers has been moved to a separate file with handlers
    - Add a string:
        default=DefaultBotProperties(parse_mode=settings.bot.PARSE_MOD)

--- app/main.py
+++ app/main.py
@@ -4,18 +4,19 @@ import logging, sys
 from aiogram import Bot, Dispatcher
 from aiogram.client.bot import DefaultBotProperties
 
-from app.handlers import greating 
 from app.core.settings import get_settings
+from app.handlers.users.routers import user_router
 
 dp = Dispatcher()
 
 
 async def main():
-    bot = Bot(token=get_settings().bot.BOT_TOKEN.get_secret_value())
+    settings = get_settings()
+    bot = Bot(token=settings.bot.BOT_TOKEN.get_secret_value(), 
+              default=DefaultBotProperties(parse_mode=settings.bot.PARSE_MOD)
+              )
     dp = Dispatcher()
-    dp.include_routers(
-        greating.router
-        )
+    dp.include_router(user_router)
 
     logging.basicConfig(level=logging.INFO, stream=sys.stdout) #настройка для вывода логов в терминал
     await dp.start_polling(bot)
