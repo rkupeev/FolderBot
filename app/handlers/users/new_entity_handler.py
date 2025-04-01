c2f190cc2b4a9e3e0d5531213cdeabe5ce7442c1
Ruslan Kupeev <rkupeev@users.noreply.github.com>
2025-04-01 23:52:55 +0300

    I started writing a step-by-step handler (with FSMStates) for accepting entries into the database from the user

--- app/handlers/users/new_entity_handler.py
+++ app/handlers/users/new_entity_handler.py
@@ -1,11 +1,30 @@
-from aiogram import Router, F
+import asyncio
+import asyncpg
+
+from datetime import datetime
+from aiogram import Bot, Dispatcher, Router, types
+from aiogram.types import Message
 from aiogram.filters import Command
-from aiogram.types import Message, ReplyKeyboardRemove
-
-from aiogram.enums import ParseMode
-
+from aiogram.fsm.state import State, StatesGroup
 from aiogram.fsm.context import FSMContext
+from aiogram.fsm.storage.memory import MemoryStorage
+
+from app.fsm.states import NoteState
 
 
+entity_router = Router()
 
+@entity_router.message(Command("add"))
+async def entity_title(message: Message, state: FSMContext):
+    await message.answer(
+        text='1️⃣Введите заголовок новой записи:',
+        reply_markup=None)
+    await state.set_state(NoteState.title)
+
+
+@entity_router.message(NoteState.content_state)
+async def entity_content(message: Message, state: FSMContext):
+    await message.answer("✅ Заголовок успешно принят!")
+    await message.answer("2️⃣Введите текст записи:")
+    await state.set_state(NoteState.content_state)
 
