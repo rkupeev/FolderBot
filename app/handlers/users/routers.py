dfb9cf3ed90606e0e666bf002e6b089079620eb3
Ruslan Kupeev <rkupeev@users.noreply.github.com>
2025-04-01 23:53:17 +0300

    added new routers

--- app/handlers/users/routers.py
+++ app/handlers/users/routers.py
@@ -2,8 +2,10 @@ from aiogram import Router
 from app.handlers.users.start_handler import start_router
 from app.handlers.users.help_handler import help_router
 from app.handlers.users.menu_handler import menu_router
+from app.handlers.users.cancel_handler import cancel_router
 
 user_router = Router()
 user_router.include_router(start_router)
 user_router.include_router(help_router)
 user_router.include_router(menu_router)
+user_router.include_router(cancel_router)
