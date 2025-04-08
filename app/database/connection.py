f8a3efd6084bbeddcaaf46d845fbfe76985a6cfe
Ruslan Kupeev <rkupeev@users.noreply.github.com>
2025-04-08 21:44:46 +0300

    Added session factory. Old code has been reviewed and almost completely removed

--- app/database/connection.py
+++ app/database/connection.py
@@ -4,19 +4,11 @@ from sqlalchemy.ext.asyncio import async_sessionmaker, AsyncSession, create_asyn
 from app.core.settings import get_settings
 from app.database.models import Base
 
-async def create_session() -> None:
-    engine = create_async_engine(
-        get_settings().database.DATABASE_URL_asyncpg,
-        echo=True
-    )
 
-    #async_session = async_sessionmaker(engine, expire_on_commit=False)
+engine = create_async_engine(
+    get_settings().database.get_db_url(),
+    echo=True)
 
-    async with engine.begin() as conn:
-        await conn.run_sync(Base.metadata.create_all)
-
-
-asyncio.run(create_session())
-
-
+async_session = async_sessionmaker(engine,
+                                   expire_on_commit=False)
 
