d996f0b4816d91d3ad6d1b30adadc54cf7a613ba
Ruslan Kupeev <rkupeev@users.noreply.github.com>
2025-04-08 15:29:38 +0300

    Minor changes:
    - Minor functional change to base class
    - Minor changes to table structure

--- app/database/models.py
+++ app/database/models.py
@@ -1,10 +1,11 @@
 from sqlalchemy import Column, Integer, BigInteger, String, Text, ForeignKey, TIMESTAMP, func
 from sqlalchemy.orm import relationship
 from sqlalchemy.orm import DeclarativeBase, mapped_column
+from sqlalchemy.ext.asyncio import AsyncAttrs
 
 
-class Base(DeclarativeBase):
-    pass
+class Base(AsyncAttrs, DeclarativeBase):
+    __abstract__ = True
 
 
 
@@ -13,6 +14,7 @@ class User(Base):
 
     id = mapped_column(BigInteger, primary_key=True, autoincrement=True)
     username = mapped_column(String(120), nullable=False, autoincrement=True)
+    added_at = mapped_column(TIMESTAMP)
 
     #relationship one-to-many with Entity
     entities = relationship("Entity", back_populates="users")
@@ -20,13 +22,13 @@ class User(Base):
 
 
 class Entity(Base):
-        __tablename__ = "entities"
+    __tablename__ = "entities"
 
-        id = mapped_column(Integer, primary_key=True, autoincrement=True)
-        title = mapped_column(String(255), nullable=False)
-        content = mapped_column(Text, nullable=False)
-        user_id = mapped_column(BigInteger, ForeignKey("users.id", ondelete="CASCADE"), nullable=False)
-        created_at = mapped_column(TIMESTAMP, server_default=func.now())
+    id = mapped_column(Integer, primary_key=True, autoincrement=True)
+    user_id = mapped_column(BigInteger, ForeignKey("users.id", ondelete="CASCADE"), nullable=False)
+    title = mapped_column(String(255), nullable=False)
+    content = mapped_column(Text, nullable=False)
+    created_at = mapped_column(TIMESTAMP)
     
-        user = relationship("User", back_populates="entities")
+    user = relationship("User", back_populates="entities")
 
