57c6ba7219b404aca9cc82606e0c4a5c23d7c968
Ruslan Kupeev <rkupeev@users.noreply.github.com>
2025-04-08 21:46:50 +0300

    A function has been created for one-time creation of tables in the database

--- app/database/database_init.py
+++ app/database/database_init.py
@@ -1,8 +1,12 @@
 
 from app.database.connection import engine
 from app.database.models import Base
+import asyncio
 
 
 async def init_db() -> None: 
     async with engine.begin() as conn:
+        #await conn.run_sync(Base.metadata.drop_all)
         await conn.run_sync(Base.metadata.create_all)
+
+asyncio.run(init_db())
