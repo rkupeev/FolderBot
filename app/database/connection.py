e25b5e79d4d3c34b4e0dfcac34d2860739b944b5
Ruslan Kupeev <rkupeev@users.noreply.github.com>
2025-04-22 16:06:11 +0300

    The connection decorator was written and bound to the corresponding handlers

--- app/database/connection.py
+++ app/database/connection.py
@@ -1,14 +1,24 @@
-import asyncio
-from sqlalchemy.ext.asyncio import async_sessionmaker, AsyncSession, create_async_engine
+from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
 
 from app.core.settings import get_settings
-from app.database.models import Base
 
 
 engine = create_async_engine(
     get_settings().database.get_db_url(),
     echo=True)
 
-async_session = async_sessionmaker(engine,
+async_session_maker = async_sessionmaker(engine,
                                    expire_on_commit=False)
+
+def connection(func):
+    async def wrapper(*args, **kwargs):
+        async with async_session_maker() as session:
+            try:
+                return await func(*args, session=session, **kwargs)
+            except Exception as e:
+                await session.rollback() #откат сессии
+                raise e
+            finally: 
+                await session.close()
+    return wrapper
 
