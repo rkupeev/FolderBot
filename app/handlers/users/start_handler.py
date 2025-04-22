19ee33a048ea44b70fabd5278e81e35e6a34d666
Ruslan Kupeev <rkupeev@users.noreply.github.com>
2025-04-22 16:08:35 +0300

    Decorator usage and answer on the command "start" with "hello" stickers

--- app/handlers/users/start_handler.py
+++ app/handlers/users/start_handler.py
@@ -1,25 +1,24 @@
-
 from aiogram import Router
-from aiogram.filters import CommandStart
+from aiogram.filters import CommandStart, Command
 from aiogram.types import Message, message
 from aiogram.fsm.context import FSMContext
 
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy import select
-from app.database.connection import async_session
+from app.database.connection import async_session_maker
 
 from app.database.models import Users
+from app.database.connection import connection
 from app.keyboards.menu_keyboard import show_menu
 
 start_router = Router()
 
-session = async_session()
-
+@connection
 async def get_or_create_user(telegram_id: int, session: AsyncSession) -> tuple[Users, bool]:
     result = await session.execute(select(Users).where(Users.telegram_id == telegram_id)) 
     user = result.scalar_one_or_none()
     if user:
-        return Users, False
+        return user, False
     else:
         new_user = Users(telegram_id=telegram_id)
         session.add(new_user)
@@ -30,7 +29,9 @@ async def get_or_create_user(telegram_id: int, session: AsyncSession) -> tuple[U
 @start_router.message(CommandStart())
 async def cmd_start(message: message, state: FSMContext):
     await state.clear()
-    user, flag = await get_or_create_user(message.from_user.id, session)
+    user, flag = await get_or_create_user(message.from_user.id)
+    
+    await message.answer_sticker('CAACAgIAAxkBAAIGNmgHhgNH7PsZMq6nR7OVCGKUVsAaAALYDwACSPJgSxX7xNp4dGuYNgQ')
 
     if flag:
         await message.answer(
@@ -42,3 +43,4 @@ async def cmd_start(message: message, state: FSMContext):
             text=f"Рады снова вас видеть, {message.from_user.full_name}!",
             reply_markup=show_menu()
             )
+        
