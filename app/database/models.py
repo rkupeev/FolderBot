290935bef64538e2af16891c56efde8ad907944d
Ruslan Kupeev <rkupeev@users.noreply.github.com>
2025-04-22 16:05:37 +0300

    Added Mapped annotations to model strings

--- app/database/models.py
+++ app/database/models.py
@@ -1,6 +1,6 @@
-from sqlalchemy import Column, Integer, BigInteger, String, Text, ForeignKey, DateTime
+from sqlalchemy import Integer, BigInteger, String, Text, ForeignKey, DateTime
 from sqlalchemy.orm import relationship
-from sqlalchemy.orm import DeclarativeBase, mapped_column
+from sqlalchemy.orm import DeclarativeBase, mapped_column, Mapped
 from sqlalchemy.ext.asyncio import AsyncAttrs
 
 from datetime import datetime
@@ -14,23 +14,23 @@ class Base(AsyncAttrs, DeclarativeBase):
 class Users(Base):
     __tablename__ = "users"
 
-    id = mapped_column(BigInteger, primary_key=True, autoincrement=True)
-    telegram_id = mapped_column(BigInteger, nullable=False)
-    added_at = mapped_column(DateTime, default=datetime.now())
+    id: Mapped[int] = mapped_column(BigInteger, primary_key=True, autoincrement=True)
+    telegram_id: Mapped[int] = mapped_column(BigInteger, nullable=False)
+    added_at: Mapped[DateTime]= mapped_column(DateTime, default=datetime.now())
 
     #relationship one-to-many with Entity
-    entities = relationship("Entities", back_populates="users")
+    entities: Mapped[list["Entities"]] = relationship("Entities", back_populates="users")
 
 
 
 class Entities(Base):
     __tablename__ = "entities"
 
-    id = mapped_column(Integer, primary_key=True, autoincrement=True)
-    user_id = mapped_column(BigInteger, ForeignKey("users.id", ondelete="CASCADE"), nullable=False)
-    title = mapped_column(String(255), nullable=False)
-    content = mapped_column(Text, nullable=False)
-    created_at = mapped_column(DateTime, default=datetime.now())
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
+    user_id: Mapped[int] = mapped_column(BigInteger, ForeignKey("users.id", ondelete="CASCADE"), nullable=False)
+    title: Mapped[str] = mapped_column(String(255), nullable=False)
+    content: Mapped[str] = mapped_column(Text, nullable=False)
+    created_at: Mapped[DateTime] = mapped_column(DateTime, default=datetime.now())
     
-    users = relationship("Users", back_populates="entities")
+    users: Mapped["Users"] = relationship("Users", back_populates="entities")
 
