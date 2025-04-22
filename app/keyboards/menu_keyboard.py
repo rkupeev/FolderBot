15d57a559870b33ab6f1685ae2fcda2cb25ea37a
Ruslan Kupeev <rkupeev@users.noreply.github.com>
2025-04-22 19:07:19 +0300

    Added a new button

--- app/keyboards/menu_keyboard.py
+++ app/keyboards/menu_keyboard.py
@@ -8,10 +8,13 @@ router = Router()
 def show_menu() -> ReplyKeyboardBuilder:
     builder = ReplyKeyboardBuilder()
     builder.add(
-        KeyboardButton(text='👤Профиль'),
-        KeyboardButton(text='🗂Записи')
+        KeyboardButton(text='👤 Профиль'),
+        KeyboardButton(text='🗂 Записи')
     )
-
+    builder.add(
+        KeyboardButton(text="ℹ️ Помощь")
+    )
+    builder.adjust(2)
     return builder.as_markup(resize_keyboard=True)
 
 
