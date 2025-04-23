e07eb9651666b3cdd5be4c91cf5955b2b6cb3bc9
Ruslan Kupeev <rkupeev@users.noreply.github.com>
2025-04-23 23:28:36 +0300

    A message handler has been written to create a new record for a user.

--- app/handlers/users/new_entity_handler.py
+++ app/handlers/users/new_entity_handler.py
@@ -1,30 +1,52 @@
-import asyncio
-import asyncpg
-
-from datetime import datetime
-from aiogram import Bot, Dispatcher, Router, types
+from aiogram import Router
 from aiogram.types import Message
 from aiogram.filters import Command
-from aiogram.fsm.state import State, StatesGroup
 from aiogram.fsm.context import FSMContext
-from aiogram.fsm.storage.memory import MemoryStorage
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy import select
 
 from app.fsm.states import NoteState
+
+from app.database.models import Users, Entities
+from app.database.connection import connection
 
 
 entity_router = Router()
 
+
+@connection 
+async def create_entity(telegram_id: int, title: str, content: str, session: AsyncSession) -> None:
+    result = await session.execute(select(Users.id).where(Users.telegram_id == telegram_id))
+    user_id = result.scalar_one()
+
+    template = Entities(user_id=user_id, title=title, content=content)
+    session.add(template)
+
+    await session.commit()
+
+
 @entity_router.message(Command("add"))
 async def entity_title(message: Message, state: FSMContext):
     await message.answer(
-        text='1️⃣Введите заголовок новой записи:',
-        reply_markup=None)
-    await state.set_state(NoteState.title)
+        text='1️⃣ Введите заголовок новой записи:')
+    await state.set_state(NoteState.title_state)
+
+
+@entity_router.message(NoteState.title_state)
+async def entity_content(messge: Message, state: FSMContext):
+    await state.update_data(title=messge.text)
+    await messge.answer("✅ Заголовок успешно принят!\n\n2️⃣Введите текст записи:")
+    await state.set_state(NoteState.content_state)
 
 
 @entity_router.message(NoteState.content_state)
-async def entity_content(message: Message, state: FSMContext):
-    await message.answer("✅ Заголовок успешно принят!")
-    await message.answer("2️⃣Введите текст записи:")
-    await state.set_state(NoteState.content_state)
+async def ending_of_adding(message: Message, state: FSMContext):
+    data = await state.get_data()
+    title = data.get("title")
+    content = message.text
+    telegram_id = message.from_user.id
 
+    await create_entity(telegram_id=telegram_id, title=title, content=content)
+    await message.answer("🥳 Запись успешно добавлена!")
+    await state.clear()
+ 
