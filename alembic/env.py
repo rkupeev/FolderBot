0051dfedce551ca697f2d01e214fd5cebbd564f9
Ruslan Kupeev <rkupeev@users.noreply.github.com>
2025-05-03 20:28:05 +0300

    Minor changes

--- alembic/env.py
+++ alembic/env.py
@@ -21,14 +21,14 @@ if config.config_file_name is not None:
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
 from app.database.models import Base
-from app.core.settings import settings
+from app.core.settings import get_settings
 target_metadata = Base.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
 # my_important_option = config.get_main_option("my_important_option")
 # ... etc.
-config.set_main_option("sqlalchemy.url", settings.database.get_db_url)
+config.set_main_option("sqlalchemy.url", get_settings().database.get_db_url())
 
 
 
