48aa586eed601f52d5130e3484b097234b29b5a7
Ruslan Kupeev <rkupeev@users.noreply.github.com>
2025-05-08 19:26:51 +0300

    Removed some routers. Rewrote existing ones.

--- app/handlers/users/routers.py
+++ app/handlers/users/routers.py
@@ -1,15 +1,9 @@
 from aiogram import Router
-from app.handlers.users.start_handler import start_router
-from app.handlers.users.help_handler import help_router
-from app.handlers.users.menu_handler import menu_router
-from app.handlers.users.cancel_handler import cancel_router
-from app.handlers.users.profile_handler import profile_router
-from app.handlers.users.new_entity_handler import entity_router
+from app.handlers.users.start import start_router
+from app.handlers.users.information import information_router
+from app.handlers.users.new_entity import entity_router
 
 user_router = Router()
 user_router.include_router(start_router)
-user_router.include_router(help_router)
-user_router.include_router(menu_router)
-user_router.include_router(cancel_router)
-user_router.include_router(profile_router)
+user_router.include_router(information_router)
 user_router.include_router(entity_router)
